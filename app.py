a2fce8043ee68fe939cac894a6b8b8571d87fae5
Ajay9704 <162597283+Ajay9704@users.noreply.github.com>
2025-03-15 22:43:26 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,8 +4,14 @@ import string
 from nltk.corpus import stopwords
 import nltk
 from nltk.stem.porter import PorterStemmer
-nltk.download('punkt')
 nltk.download('punkt_tab')
+nltk.download('punkt')        
+nltk.download('stopwords')  
+nltk.download('wordnet')      
+nltk.download('omw-1.4')     
+nltk.download('averaged_perceptron_tagger') 
+nltk.download('maxent_ne_chunker')         
+nltk.download('words')
 ps = PorterStemmer()
 
 
